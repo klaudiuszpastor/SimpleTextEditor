417c7d31318f198a22a87637cbd51dfaeb827241
Klaudiusz Pastor <pastor.klo12@gmail.com>
2024-08-14 19:33:24 +0200

    some keys

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -86,8 +89,13 @@ int editorReadKey() {
                 if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
                 if (seq[2] == '~') {
                     switch(seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
         } else {
@@ -96,7 +104,14 @@ int editorReadKey() {
                 case 'B': return ARROW_DOWN;
                 case 'C': return ARROW_RIGHT;
                 case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
+        }
+    } else if (seq[0] == '0') {
+        switch (seq[1]) {
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
         }
     }
 
@@ -251,7 +266,24 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H",3);
             exit(0);
             break;
-            
+        
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while (times--) {
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                }
+            }
+            break;
+
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
